fc4edc6d1ff199c141699bda0f0a368edd28b91b
PadNim14 <npadmana@purdue.edu>
2021-10-07 14:39:43 -0400

    updated completion page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM, { createPortal } from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 import "survey-react/survey.css";
 import * as Survey from "survey-react";
@@ -10,26 +10,22 @@ import reportWebVitals from './reportWebVitals';
 
 // var surveyJSON = {title:"Patient Basic Assessment",description:"Obtains basic patient information",pages:[{name:"page1",elements:[{type:"text",name:"question10",title:"Full Name",isRequired:true},{type:"text",name:"date1",title:"Date: (format dd/mm/yyyy)",isRequired:true,inputType:"datetime"},{type:"rating",name:"question2",title:"I weigh myself everyday.",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question1",title:"If my shortness of breath increases, I contact my doctor or nurse.",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question3",title:"If my feet/legs become more swollen than usual, I contact my doctor or nurse. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question4",title:"If I gain 2 kg in one week, I contact my doctor or nurse. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question5",title:"If I experience increased fatigue, I contact my doctor or nurse. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question6",title:"I eat a low salt diet. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question7",title:"I take my medication as prescribed. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question8",title:"I exercise regularly.",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"}],description:"Click 1 if you strongly disagree with the statement; click 5 if you strongly agree with the statement. Otherwise, rate your answer accordingly.\n"}],showCompletedPage:false,navigateToUrl:"https://padnim14.github.io/survey-app/",navigateToUrlOnCondition:[{expression:"Take Survey Again ",url:"https://padnim14.github.io/survey-app/"}]}
 
-function sendDataToServer(survey) {
-    survey.sendResult('4bd65df1-3324-4d20-b190-c4b35440589d');
-}
+// function sendDataToServer(survey) {
+//     survey.sendResult('4bd65df1-3324-4d20-b190-c4b35440589d');
+// }
 
 // ReactDOM.render(
 //     <Survey.Survey json={ surveyJSON } onComplete={ sendDataToServer } />, document.getElementById("surveyContainer"));
 Survey.StylesManager.applyTheme("orange");
 
-var surveyJSON = {"title":"Patient Basic Assessment","description":"Obtains basic patient information","pages":[{"name":"page1","elements":[{"type":"text","name":"question10","title":"Full Name"},{"type":"text","name":"date1","title":"Date:","isRequired":true,"inputType":"datetime"},{"type":"rating","name":"question2","title":"I weigh myself everyday."},{"type":"rating","name":"question1","title":"If my shortness of breath increases, I contact my doctor or nurse."},{"type":"rating","name":"question3","title":"If my feet/legs become more swollen than usual, I contact my doctor or nurse. "},{"type":"rating","name":"question4","title":"If I gain 2 kg in one week, I contact my doctor or nurse. "},{"type":"rating","name":"question5","title":"If I experience increased fatigue, I contact my doctor or nurse. "},{"type":"rating","name":"question6","title":"I eat a low salt diet. "},{"type":"rating","name":"question7","title":"I take my medication as prescribed. "},{"type":"rating","name":"question8","title":"I exercise regularly."}],"title":"Patient Vital Survey","description":"Click 1 if you strongly disagree with the statement; click 5 if you strongly agree with the statement. Otherwise, rate your answer accordingly.\n"}]}
+var surveyJSON = {title:"Patient Basic Assessment",description:"Obtains basic patient information",completedHtml:"<!DOCTYPE html>\n<html lang=\"en\">\n    <head>\n        <h1>Thank you for completing the survey </h1>\n        <a href=\"https://padnim14.github.io/survey-app/\"> Take survey again </a>\n    </head>\n</html>",pages:[{name:"page1",elements:[{type:"text",name:"question10",title:"Full Name",isRequired:true},{type:"text",name:"date1",title:"Date: (format dd/mm/yyyy)",isRequired:true,inputType:"datetime"},{type:"rating",name:"question2",title:"I weigh myself everyday.",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question1",title:"If my shortness of breath increases, I contact my doctor or nurse.",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question3",title:"If my feet/legs become more swollen than usual, I contact my doctor or nurse. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question4",title:"If I gain 2 kg in one week, I contact my doctor or nurse. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question5",title:"If I experience increased fatigue, I contact my doctor or nurse. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question6",title:"I eat a low salt diet. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question7",title:"I take my medication as prescribed. ",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"},{type:"rating",name:"question8",title:"I exercise regularly.",isRequired:true,minRateDescription:"Low",maxRateDescription:"High"}],description:"Click 1 if you strongly disagree with the statement; click 5 if you strongly agree with the statement. Otherwise, rate your answer accordingly.\n"},{name:"page2",elements:[{type:"html",name:"question9"}]}],showProgressBar:"top",progressBarType:"questions"}
 
-// function sendDataToServer(survey) {
-//     //send Ajax request to your web server.
-//     alert("The results are:" + JSON.stringify(survey.data));
-// }
+function sendDataToServer(survey) {
+    survey.sendResult('4bd65df1-3324-4d20-b190-c4b35440589d');
+}
 
 ReactDOM.render(
     <Survey.Survey json={ surveyJSON } onComplete={ sendDataToServer } />, document.getElementById("surveyContainer"));
-
-  
-
 //     ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
